0b9aeb7ffc7bfb4535947e7c07e4f12bec273d71
bananarama23 <philksmith@googlemail.com>
2026-05-17 21:55:17 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'wt-shell-v193-tracker-api-no-store';
+const SW_VERSION = 'wt-shell-v194-tracker-mailjet-dashboard';
 const SHELL_CACHE = `wt-shell-${SW_VERSION}`;
 const WT_BLOCK_PUBLIC_GITHUB_PAGES = /\.github\.io$/i.test((self.location && self.location.hostname) || '');
 const OFFLINE_URLS = [
