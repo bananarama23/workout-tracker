6fe293a5f234caad8c04fa083ae4db362dd60fb1
bananarama23 <philksmith@googlemail.com>
2026-05-17 17:21:09 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'wt-shell-v186-tracker-actions';
+const SW_VERSION = 'wt-shell-v187-tracker-web-search';
 const SHELL_CACHE = `wt-shell-${SW_VERSION}`;
 const WT_BLOCK_PUBLIC_GITHUB_PAGES = /\.github\.io$/i.test((self.location && self.location.hostname) || '');
 const OFFLINE_URLS = [
